70f5dad8fdc4ecfb24a69cdfad7f882f9b661859
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2023-11-15 20:23:33 +0100

    something

--- larpex-web-fe/src/ucEventOrganiserPanel/views/EventListView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/EventListView.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import EventView from "./EventView.tsx";
+import { useState } from "react";
+import PropTypes from 'prop-types';
 
-interface EventListViewProps { }
-const EventListView: React.FC<EventListViewProps> = () => {
+
+interface EventListViewProps {
+    events: {
+      name: string; // I assume ID is defined elsewhere
+      desc: string;
+      id: string;
+    }[
+    ]
+  }
+
+const EventListView: React.FC<EventListViewProps> = ({events}) => {
+
+    
+    const list = ["raz","dwa","trzy"]
 
     return (
         <>
-            <div>eyo list</div>
-            <EventView/>
+            <div><h4>name  |  description  | ID</h4></div>
+            <div className="EventListView">
+                {events.map((item) => (
+                    <EventView name={item.desc} desc={item.desc} id={item.id}/>
+                ))                     
+                    
+                } 
+            </div>
         </>
     );
+
+                
 }
 
 export default EventListView;
--- larpex-web-fe/src/ucEventOrganiserPanel/views/EventOrganiserPanelView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/EventOrganiserPanelView.tsx
@@ -1,49 +1,47 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import EventListView from "./EventListView.tsx";
-import {EventViewDto} from "../viewModels/EventViewModel.ts";
-import {getEventsData} from "../logic/EventOrganiserPanelService.ts";
+import { useState } from "react";
+
 
 interface EventOrganiserPanelViewProps { }
+
+
+
 const EventOrganiserPanelView: React.FC<EventOrganiserPanelViewProps> = () => {
 
-
-    const [data, setData] = useState<EventViewDto[] | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await getEventsData();
-                setData(result);
-                console.log("promise");
-                console.log(result);
-                console.log("data");
-                console.log(data);
-            } catch (error) {
-                // Handle errors here
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, []);
+    const [events] = useState([
+        {
+            name: 'event 0',
+            desc: 'description 0',
+            id: '0',
+        },
+        {
+            name: 'event 1',
+            desc: 'description 1',
+            id: '1',
+        },
+        {
+            name: 'event 2',
+            desc: 'description 2',
+            id: '2',
+        },
+        {
+            name: 'event 3',
+            desc: 'description 3',
+            id: '3',
+        },
+        {
+            name: 'event 4',
+            desc: 'description 4',
+            id: '4',
+        },
+    ]);
 
     return (
         <>
-            <div>
-                eyo panel
-                <p>data</p>
-                {loading ? (
-                    <p>Loading...</p>
-                ) : (
-                    <div>
-                        {/* Display your data here */}
-                        <p>Data: {data ? data[0].status : '-'}</p>
-                    </div>
-                )}
-            </div>
+            <div>eyo panel</div>
+            <EventListView events={events}/>
 
-            <EventListView/>
         </>
     );
 }
