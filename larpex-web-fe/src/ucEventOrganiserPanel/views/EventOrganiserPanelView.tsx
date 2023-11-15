bf471b00a69fd9218f535f02d17103024680bc60
p-tecza <pawel.tecza.pt@gmail.com>
2023-11-15 18:42:47 +0100

    events basic req

--- larpex-web-fe/src/ucEventOrganiserPanel/views/EventOrganiserPanelView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/EventOrganiserPanelView.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import EventListView from "./EventListView.tsx";
+import {EventViewDto} from "../viewModels/EventViewModel.ts";
+import {getEventsData} from "../logic/EventOrganiserPanelService.ts";
 
 interface EventOrganiserPanelViewProps { }
 const EventOrganiserPanelView: React.FC<EventOrganiserPanelViewProps> = () => {
 
 
+    const [data, setData] = useState<EventViewDto[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await getEventsData();
+                setData(result);
+            } catch (error) {
+                // Handle errors here
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
-            <div>eyo panel</div>
+            <div>
+                eyo panel
+                <p>data</p>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div>
+                        {/* Display your data here */}
+                        <p>Data: {data ? data[0].status : '-'}</p>
+                    </div>
+                )}
+            </div>
+
             <EventListView/>
         </>
     );
