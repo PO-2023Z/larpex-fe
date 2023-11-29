ae9320d78bb17dd825135fd5f882d823a9c05aad
p-tecza <pawel.tecza.pt@gmail.com>
2023-11-29 18:14:40 +0100

    small view changes

--- larpex-web-fe/src/ucEventOrganiserPanel/views/EventListView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/EventListView.tsx
@@ -19,13 +19,13 @@ const EventListView: React.FC<EventListViewProps> = ({events}) => {
                             <span className="d-inline-block text-truncate wdh-15"> Name</span>
                             <span className="d-inline-block text-truncate wdh-30"> &nbsp;Description</span>
                             <span className="d-inline-block text-truncate wdh-7">  &nbsp;Entry </span>
-                            <span className="d-inline-block text-truncate wdh-15"> &nbsp;Location </span>
+                            <span className="d-inline-block text-truncate wdh-7"> &nbsp;Price </span>
                             <span className="d-inline-block text-truncate wdh-10"> &nbsp;Date </span>
                         </h5>
                     </li>
                     {events.map((item) => (
                         <li className="list-group-item organiser-panel-content-2">
-                            <EventView name={item.name} desc={item.descriptionForEmployee} currentlySignedPlayers={item.currentlySignedPlayers} maxPlayerLimit={item.eventSettings.maxPlayerLimit} id={item.id} location={item.location} startDate={item.startDate}/>
+                            <EventView data={item}/>
                         </li>
                     ))
                     }
--- larpex-web-fe/src/ucEventOrganiserPanel/views/EventView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/EventView.tsx
@@ -2,33 +2,30 @@ import React from "react";
 import InvitationButtonView from "./InvitationButtonView.tsx";
 import EditEventButtonView from "./EditEventButtonView.tsx";
 import "./EventView.css";
+import {EventViewDto} from "../viewModels/EventViewModel.ts";
 
 interface EventViewProps {
-    name: string;
-    desc: string;
-    id: string;
-    currentlySignedPlayers: number;
-    maxPlayerLimit: number;
-    location: string;
-    startDate: string;
+    data: EventViewDto;
 }
 
 
 const EventView: React.FC<EventViewProps> = (EventViewProps) => {
 
-    const strDate=EventViewProps.startDate.slice(0,10)
+
     return (
 
         <div>
             <div className="organiser-panel-content">
                 <h5>
-                    <span className="d-inline-block text-truncate wdh-15">{EventViewProps.name}</span>
-                    <span className="d-inline-block text-truncate wdh-30">&nbsp;{EventViewProps.desc}</span>
-                    <span className="d-inline-block text-truncate wdh-7"> &nbsp;{EventViewProps.currentlySignedPlayers}/{EventViewProps.maxPlayerLimit}</span>
-                    <span className="d-inline-block text-truncate wdh-15">&nbsp;{EventViewProps.location}</span>
-                    <span className="d-inline-block text-truncate wdh-10">&nbsp;{strDate}</span>
+                    <span className="d-inline-block text-truncate wdh-15">{EventViewProps.data.name}</span>
+                    <span className="d-inline-block text-truncate wdh-30">&nbsp;{EventViewProps.data.descriptionForEmployee}</span>
+                    <span className="d-inline-block text-truncate wdh-7">
+                        &nbsp;{EventViewProps.data.currentlySignedPlayers}/{EventViewProps.data.eventSettings.maxPlayerLimit}
+                    </span>
+                    <span className="d-inline-block text-truncate wdh-7">&nbsp;{EventViewProps.data.pricePerUser}</span>
+                    <span className="d-inline-block text-truncate wdh-15">&nbsp;{EventViewProps.data.startDate.substring(0,10)}</span>
                     <span className="d-inline-block text-truncate wdh-20">
-                        <EditEventButtonView eventId={EventViewProps.id}/>
+                        <EditEventButtonView eventId={EventViewProps.data.id}/>
                         <InvitationButtonView/>
                     </span>
                 </h5>
