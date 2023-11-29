012499ace814a11bcc2a7bfbb41a3916aded06e8
p-tecza <pawel.tecza.pt@gmail.com>
2023-11-29 22:43:00 +0100

    pl lang for event organiser panel fix

--- larpex-web-fe/src/ucEventOrganiserPanel/views/InvitationButtonView.tsx
+++ larpex-web-fe/src/ucEventOrganiserPanel/views/InvitationButtonView.tsx
@@ -4,7 +4,7 @@ interface InvitationButtonViewProps{}
 
 const InvitationButton: React.FC<InvitationButtonViewProps> = () => {
     return (
-        <button type="button" className="btn btn-primary primary-button ml-1">Send Invitation</button>
+        <button type="button" className="btn btn-primary primary-button ml-1">Zaproś</button>
     );
 }
 
